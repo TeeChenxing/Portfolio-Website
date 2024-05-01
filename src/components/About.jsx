1aa43d44cae6feca5d3569532b5acc9584b22319
teera tesharojanasup <teerareal74@gmail.com>
2024-05-01 01:07:00 -0400

    Changed service card colors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,11 @@ const ServiceCard = ({ index, title, icon }) => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: true, amount: 0.25 }}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      className="w-full purple-gray-gradient p-[3px] rounded-[20px] shadow-card"
     >
       <div
         options={{ max: 45, scale: 1, speed: 450 }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        className="bg-tertiary bg-opacity-80 rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-white text-[20px] font-bold text-center">
